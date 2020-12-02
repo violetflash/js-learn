1a004f2e01962f26075d4a6231fc908d2d68d813
Alex Grobovsky <ktotaam@mail.ru>
2020-12-02 22:48:37 +0500

    save

--- freeman-book/11.bubble-scores/main.js
+++ freeman-book/11.bubble-scores/main.js
@@ -6,6 +6,23 @@ var scores = [
     46, 31, 57, 52, 44, 18,
     41, 53, 55, 61, 51, 44];
 
+var costs = [
+    .25, .27, .25, .25, .25, .25,
+    .33, .31, .25, .29, .27, .22,
+    .31, .25, .25, .33, .21, .25,
+    .25, .25, .28, .25, .24, .22,
+    .20, .25, .30, .25, .24, .25,
+    .25, .25, .27, .25, .26, .29];
+
+function effectiveScore(scores, costs) {
+    var cheapest = 0;
+    for (let i = 0; i < scores.length; i++) {
+        if (scores[i] == maxScore(scores)) {
+
+        }
+    }
+}
+
 function printScores(scores) {
     for (let i = 0; i < scores.length; i++) {
         document.write("Bubble solution #" + [i] + " score: " + scores[i] + "<br>");
@@ -22,35 +39,16 @@ function maxScore(scores) {
     return max;
 }
 
-function maxScoreIndexes(score) {
+function maxScoreIndexes(scores) {
     var best_result = [];
-}
-
-function report() {
-    var max = 0;
-    var best_result = [];
-
-    //find max score
     for (let i = 0; i < scores.length; i++) {
-        document.write("Bubble (FOR) solution #" + [i] + " score: " + scores[i] + "<br>");
-        if (scores[i] > max) {
-            max = scores[i];
-        }
-    }
-
-    //find indexes of max scores
-    for (let i = 0; i < scores.length; i++) {
-        if (scores[i] == max) {
+        if (scores[i] == maxScore(scores)) {
             best_result[best_result.length] = i;
             //or
             // best_result.push(i)
         }
     }
-    document.write("===========================" + "<br><br>");
-    document.write("Bubble tests: " + scores.length + "<br>");
-    document.write("Highest bubble score: " + max + "<br>");
-    document.write("Solutions with highest score: ")
-    hashificator(best_result);
+    return best_result;
 }
 
 function hashificator(elem) {
@@ -59,4 +57,13 @@ function hashificator(elem) {
     }
 }
 
-report();
+function report(scores) {
+    printScores(scores);
+    document.write("===========================" + "<br><br>");
+    document.write("Bubble tests: " + scores.length + "<br>");
+    document.write("Highest bubble score: " + maxScore(scores) + "<br>");
+    document.write("Solutions with highest score: ")
+    hashificator(maxScoreIndexes(scores));
+}
+
+report(scores);
