c03ec7aa134808c8deac17cbc9eacc394be2111f
Alex Grobovsky <ktotaam@mail.ru>
2020-12-16 01:26:49 +0500

    save

--- freeman-book/05.Functions/01.passengers/main.js
+++ freeman-book/05.Functions/01.passengers/main.js
@@ -7,13 +7,64 @@ var passengers = [
 
 var blacklist = ["Dr. Evil", "Sue Property"];
 
-blacklistFind = function(passengers) {
-    for (let i = 0; i < passengers.length; i++) {
-        document.write(passengers[i].name + "<br>");
-        if (blacklist.indexOf(passengers[i].name) > 0) {
-            document.write(passengers[i] + " is blacklisted!")
-        }
-    }
+//если пассажир в черном списке - вернёт true
+function checkBlacklisted(passenger) {
+    return blacklist.indexOf(passenger) >= 0;
+
 }
 
-blacklistFind(passengers);
+//если пассажир не оплатил билет - вернет true
+function checkNotPaid(passenger) {
+    return !passenger.paid;
+}
+
+//Проверка пассажира. Если тестовая функция вернет true, значит вылет запрещён
+function processPassengers(passengers, testPassenger) {
+    for (let i = 0; i < passengers.length; i++) {
+        if (testPassenger(passengers[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+var flyOrNot = processPassengers(passengers, checkBlacklisted);
+
+var checkPaid = processPassengers(passengers, checkNotPaid);
+
+if (flyOrNot || checkPaid) {
+    console.log("We cant fly");
+}
+
+function printPassenger(passenger) {
+    var option;
+    if (passenger.paid === true) {
+        option = "payed"
+    } else {
+        option = "not payed"
+    }
+    console.log(passenger.name + " - " + option);
+}
+
+processPassengers(passengers, printPassenger);
+
+function fun(echo) {
+    console.log(echo);
+}
+
+fun("hello");
+function boo(aFunction) {
+    aFunction("boo");
+}
+boo(fun);
+fun(boo);
+
+var moreFun = fun;
+moreFun("hello again");
+
+function echoMaker() {
+    return fun;
+}
+var bigFun = echoMaker();
+bigFun("Is there an echo?");
+
