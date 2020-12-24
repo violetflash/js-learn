e7a1e1483aa6830211c6d4c2b8790d547813ccc5
Alex Grobovsky <ktotaam@mail.ru>
2020-12-24 13:00:26 +0500

    save

--- my mini-programs/01.guess-number/main.js
+++ my mini-programs/01.guess-number/main.js
@@ -1,14 +1,23 @@
+var limit = prompt("От 0 до какого числа будем загадывать?", 50);
+
 function guessNumber(number) {
+    var count = 0;
     while (true) {
-        var guess = prompt("Угадай, какое число от 0 до 20 я загадал?");
+        count++;
+        var guess = prompt(`Угадай, какое число от 0 до ${limit} я загадал?`, '');
         if (isNaN(guess)) {
             alert("Принимаются только числа!")
-        } else if (Number(guess) === secret) {
-            return alert("Угадал!");
-        } else {
-            alert("Не угадал! Попробуй еще раз!");
+        } else if (+guess === number) {
+            return alert(`Угадал! Попыток понадобилось: ${count}. `);
+        } else if (+guess > number) {
+            alert("Меньше!");
+        } else if (+guess < number) {
+            alert("Больше!");
         }
     }
 }
 
-guessNumber(16);
+var secret = Math.floor(Math.random() * (+limit + 1));
+console.log(secret);
+
+guessNumber(secret);
