8b7073e5d1ea02aa0084160cbe1f247650a9249c
Alex Grobovsky <ktotaam@mail.ru>
2021-02-01 00:03:11 +0500

    still working on openweather api

--- it-gid/04_homework/script.js
+++ it-gid/04_homework/script.js
@@ -1,74 +1,173 @@
 let weather_option = document.querySelector(".weather__option");
+let baseLink = `http://api.openweathermap.org/data/2.5/`;
+let city = document.querySelector(".weather__city");
 let method = `weather`;
-let days = document.createElement("label");
-days.style.display = "block";
-days.style.marginTop = "10px";
-days.innerHTML = `кол-во дней (от 1 до 16) 
-<select class="weather__days">
+let hoursNum = 1;
+
+document.querySelector(".date").textContent = getDate();
+window.onload = getWeather;
+
+function getDate() {
+  let date = new Date();
+  let day, month = "";
+
+  switch (date.getDay()) {
+    case 0: {
+      day = "воскресенье";
+      break;
+    }
+    case 1: {
+      day = "понедельник";
+      break;
+    }
+    case 2: {
+      day = "вторник";
+      break;
+    }
+    case 3: {
+      day = "среда";
+      break;
+    }
+    case 4: {
+      day = "четверг";
+      break;
+    }
+    case 5: {
+      day = "пятница";
+      break;
+    }
+    case 6: {
+      day = "суббота";
+      break;
+    }
+    case 7: {
+      day = "воскресенье";
+      break;
+    }
+  }
+  switch (date.getMonth()) {
+    case 0: {
+      month = "января";
+      break;
+    }
+    case 1: {
+      month = "февраля";
+      break;
+    }
+    case 2: {
+      month = "марта";
+      break;
+    }
+    case 3: {
+      month = "апреля";
+      break;
+    }
+    case 4: {
+      month = "мая";
+      break;
+    }
+    case 5: {
+      month = "июня";
+      break;
+    }
+    case 6: {
+      month = "июля";
+      break;
+    }
+    case 7: {
+      month = "августа";
+      break;
+    }
+    case 8: {
+      month = "сентября";
+      break;
+    }
+    case 9: {
+      month = "октября";
+      break;
+    }
+    case 10: {
+      month = "ноября";
+      break;
+    }
+    case 11: {
+      month = "декабря";
+      break;
+    }
+  }
+
+  return `${date.getDate()} ${month} ${date.getFullYear()}, ${day}`;
+}
+
+function getWeather() {
+  fetch(`${baseLink}${method}?id=${city.value}&cnt=${hoursNum}&appid=ef055dc2a3ce7e62285e867ad3dd0302`)
+    .then(function(resp) {
+      return resp.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      // document.querySelector(".weather__city").textContent = data.name;
+      document.querySelector(".weather__temp").innerHTML = Math.round(data.main.temp - 273) + " " + "&#8451";
+      document.querySelector(".weather__clouds").textContent = data.weather[0].description;
+      document.querySelector(".weather__humidity").textContent = data.main.humidity + " %";
+      document.querySelector(".weather__wind").textContent = Math.round(data.wind.speed) + " м/с";
+      let img = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      document.querySelector(".weather__clouds-img").setAttribute("src", img);
+    })
+    .catch(function() {
+      //catch any errors
+    });
+}
+
+function createSelect() {
+  let hours = document.createElement("div");
+  hours.className = "select-label";
+  hours.style.display = "block";
+  hours.style.marginTop = "10px";
+  hours.innerHTML = `кол-во часов (от 1 до 3) 
+<select class="weather__hours">
   <option value="1">1</option>
   <option value="2">2</option>
   <option value="3">3</option>
-  <option value="4">4</option>
-  <option value="5">5</option>
-  <option value="6">6</option>
-  <option value="7">7</option>
-  <option value="8">8</option>
-  <option value="9">9</option>
-  <option value="10">10</option>
-  <option value="11">11</option>
-  <option value="12">12</option>
-  <option value="13">13</option>
-  <option value="14">14</option>
-  <option value="15">15</option>
-  <option value="16">16</option>
 </select>`;
-
-
-
-weather_option.onchange = chooseMethod(weather_option, days);
-
-function chooseMethod(select) {
-    if (select.value === "current") {
-        method = `weather`;
-        if ( document.querySelector("label") ) {
-            document.querySelector("label").remove();
-        }
-    } else if (select.value === "forecast" && !document.querySelector(".weather__days")) {
-        method = `forecast`;
-        select.after(days);
-    }
+  // hours.innerHTML += `<button class="weather__btn">Обновить</button>`
+  return hours;
 }
 
-let daysNum;
+function createHoursForecast() {
+  let newValue = document.createElement('div');
+  newValue.className = "new-value";
+  newValue.innerHTML = "test";
+  let values = document.querySelectorAll('.value');
+  for (let j = 0; j < values.length ; j++) {
+    values[j].after(newValue);
+  }
 
-if ( document.querySelector(".weather__days") ) {
-    let daysInput = document.querySelector(".weather__days");
-
-    daysInput.onchange = () => {
-        daysNum = daysInput.value;
-        console.log(daysNum);
-    }
 }
 
+weather_option.addEventListener('change', function(e) {
+  if ( weather_option.value === "current" && document.querySelector('.select-label') ) {
+    document.querySelector('.select-label').remove();
+    method = `weather`;
+    // hoursNum = 1;
+  } else if ( weather_option.value === "forecast" ) {
+    method = `forecast`;
+    weather_option.after(createSelect());
+    let hoursSelect = document.querySelector('.weather__hours');
+    hoursSelect.addEventListener('change', function(e) {
+      if ( document.querySelector('.new-value') ) {
+        document.querySelector('.new-value').remove();
+      }
+      for (let i = 0; i < hoursSelect.value; i++) {
+        createHoursForecast();
+      }
+      hoursNum = hoursSelect.value;
+      getWeather();
+    });
+  }
+  getWeather();
+});
 
-let city = document.querySelector(".weather__city");
-let baselink = `http://api.openweathermap.org/data/2.5/`;
-
-document.querySelector('.weather__city').onchange = () => {
-    fetch(`${baselink}${method}?id=${city.value}&cnt=${daysNum}&appid=ef055dc2a3ce7e62285e867ad3dd0302`)
-        .then(function (resp) {
-            return resp.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            // document.querySelector(".weather__city").textContent = data.name;
-            document.querySelector(".weather__temp").innerHTML = Math.round(data.main.temp - 273) + " " + "&#8451";
-            document.querySelector(".weather__clouds").innerHTML = data.weather[0].description;
-            document.querySelector(".weather__humidity").innerHTML = data.main.humidity + " %";
-            let img = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-            document.querySelector(".weather__clouds-img").setAttribute("src", img);
-        })
-        .catch(function () {
-            //catch any errors
-        });
-}
+city.addEventListener('change', function(e) {
+  getWeather();
+});
