f8f797828040ecf963a6f7b219d62b5283a7de41
Alex Grobovsky <ktotaam@mail.ru>
2021-02-10 01:01:48 +0500

    save

--- Glo/course/dz3/02/main.js
+++ Glo/course/dz3/02/main.js
@@ -1,8 +1,15 @@
-let obj = {
-
-};
-
-obj.ask = () => {
-  let str = prompt("Введите любое слово");
-
+let obj = {};
+let str;
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
+
+do {
+  str = prompt("Введите любое слово");
+  if (str) str = str.trim();
+
+} while ( isNumber(str) );
+
+obj += str;
+
+console.log(obj);
