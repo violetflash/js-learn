dc3575ddd080fb5c7c1f1e56600bbd596baea068
Alex Grobovsky <ktotaam@mail.ru>
2021-03-02 15:48:43 +0500

    save

--- it-gid/unit_20/unit_20.js
+++ it-gid/unit_20/unit_20.js
@@ -14,8 +14,9 @@ document.querySelector('.i-1').addEventListener('keydown', t1);
 /*  Дан input .i-2. Напишите функцию t2, которая выводит в .out-2 код символа и возвращает его. */
 
 function t2(e) {
-    document.querySelector('.out-2').textContent = e.which;
-    return e.which;
+    console.log(e)
+    document.querySelector('.out-2').textContent = e.code;
+    return e.code;
 }
 
 // ваше событие здесь!!!
@@ -23,43 +24,53 @@ document.querySelector('.i-2').addEventListener('keydown', t2);
 
 
 // Task 3 ============================================
-/*  Дан input .i-3. Напишите функцию t3, которая выводит на страницу true если введен символ и false если цифра. Для определения - используйте код клавиши. */
+/*  Дан input .i-3. Напишите функцию t3, которая выводит на страницу true если введен символ и false если цифра.
+Для определения - используйте код клавиши. */
 
 let w3 = 75;
 
-function t3() {
-
+function t3(e) {
+    console.log(e.code['length']);
+    document.querySelector('.out-3').textContent = ( e.code['length'] === 4 )
+    // document.querySelector('.out-3').textContent = isNaN(e.key);
 }
 
 // ваше событие здесь!!!
+document.querySelector('.i-3').addEventListener('keydown', t3);
 
 
 // Task 4 ============================================
 /*  Дан input .i-4. Напишите функцию t4, которая выводит в .out-4 только символы в нижнем регистре. Т.е. ввели ab4Bci в out получаем ab4bci. */
 
 function t4() {
-
+    document.querySelector('.out-4').textContent = document.querySelector('.i-4').value.toLowerCase();
 }
 
 // ваше событие здесь!!!
+document.querySelector('.i-4').addEventListener('input', t4);
 
 // Task 5 ============================================
 /*  Дан input .i-5. Напишите функцию t5, которая выводит в .out-5 все вводимые символы в верхнем регистре. Т.е. пользователь ввел AbCd и функция выведет ABCD. */
 
 function t5() {
-
+    document.querySelector('.out-5').textContent = document.querySelector('.i-5').value.toUpperCase();
 }
 
 // ваше событие здесь!!!
+document.querySelector('.i-5').addEventListener('input', t5);
 
 // Task 6 ============================================
 /*  Дан input .i-6. Напишите функцию t6, которая выводит в .i-6 только символы в нижнем регистре.  */
 
-function t6() {
-
+function t6(e) {
+    let out = '';
+    if ( isNaN(e.key) ) out += e.key;
+    console.log(out);
+    // document.querySelector('.i-6').value += e.key;
 }
 
 // ваше событие здесь!!!
+document.querySelector('.i-6').addEventListener('input', t6);
 
 
 // Task 7 ============================================
@@ -71,6 +82,7 @@ function t7() {
 }
 
 // ваше событие здесь!!!
+document.querySelector('.i-7').addEventListener('keydown', t7);
 
 // Task 8 ============================================
 /*  Дан input .i-8. Напишите функцию t8, которая выводит в .out-8 вводимый в input текст, но заменяет i на 1, o на 0, l на 7. */
@@ -102,7 +114,7 @@ function t10() {
 // ваше событие здесь!!!
 
 // Task 11 ============================================
-/*  Проект. 
+/*  Проект.
 1. Выполните в html верстку клавиш клавиатуры. Сверстайте – блок цифровых клавиш от 1 до 0. И ряд клавиш q – p. Добавьте клавишу левый shift, левый  alt, левый ctrl,  пробел, enter.
 2. Добавьте на input .i-11 событие onkeypress или onkeyup или onkeydown ( по вашему выбору). Когда событие происходит ( ввод символа в input) необходимо подсветить ( добавить класс active) клавише с таким символом. Со всех остальных клавиш – удалить класс active.
 3. Если вводится следующий символ – повторить удаление active и подсветить клавишу с введенным символом.
@@ -114,4 +126,3 @@ function t11() {
 }
 
 // ваше событие здесь!!!
-
