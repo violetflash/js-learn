99cf1d07b3baa035fa302e8ba1b320b14a206453
Alex Grobovsky <ktotaam@mail.ru>
2021-03-11 09:49:16 +0500

    save

--- useful_code/functions.js
+++ useful_code/functions.js
@@ -3,13 +3,13 @@ let isNumber = function(n){
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-
+//=====================================================
 //обрезание строки
 function truncate(str, maxlength) {
   return ( str.length > maxlength ) ? str.slice(0, maxlength - 1) + "..." : str;
 }
 
-
+//=====================================================
 //Fisher Yates Shuffle
 function shuffle(arr) {
   //loop over array backwards
@@ -21,3 +21,25 @@ function shuffle(arr) {
     console.log(arr);
   }
 }
+
+
+//=====================================================
+//Higher order function
+//returns another function
+// (function factory)
+
+function makeBetweenFunc(min, max) {
+  return function(value) {
+    return value >= min && value <= max;
+  }
+}
+
+const isAdult = makeBetweenFunc(18, 90);
+const isChild = makeBetweenFunc(0, 18);
+
+console.log(isAdult(17));
+console.log(isChild(17));
+
+const isGoodTemp = makeBetweenFunc(0, 23);
+
+console.log(isGoodTemp(17));
