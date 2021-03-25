3e1209a027e048bb7d6d3a28218a83510becdb97
Alex Grobovsky <ktotaam@mail.ru>
2021-03-25 12:57:03 +0500

    save

--- Bootcamp/projects/timer/app.js
+++ Bootcamp/projects/timer/app.js
@@ -1,69 +1,21 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton, callbacks) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    if ( callbacks ) {  //optional argument
-      this.onStart = callbacks.onStart;
-      this.onTick = callbacks.onTick;
-      this.onComplete = callbacks.onComplete;
-    }
-
-    this.startButton.addEventListener('click', this.start)
-    this.pauseButton.addEventListener('click', this.pause)
-  }
-
-  start = () => {
-    if (this.onStart) this.onStart();
-    if (this.timeRemaining > 0) this.tick();
-    this.interval = setInterval(this.tick, 1000);
-    this.disableStart();
-  }
-
-  pause = () => {
-    clearInterval(this.interval);
-    this.ableStart();
-  }
-
-  tick = () => {
-    if ( this.timeRemaining <= 0 ) {
-      if (this.onComplete) this.onComplete();
-      this.pause();
-    } else {
-      //calling method without the parentheses because of using get keyword
-      this.timeRemaining = this.timeRemaining - 1; // value automatically goes as an argument to setter call
-      console.log(this.timeRemaining)
-    }
-  }
-
-  get timeRemaining() {
-    return this.durationInput.value;
-  }
-
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-
-  disableStart() {
-    this.startButton.setAttribute('disabled', 'true');
-  }
-
-  ableStart() {
-    this.startButton.removeAttribute('disabled');
-  }
-}
-
 const durationInput  = document.querySelector('#duration');
 const startButton  = document.querySelector('#start');
 const pauseButton  = document.querySelector('#pause');
+const circle = document.querySelector('#circle');
 
+const perimeter = circle.getAttribute('r') * Math.PI * 2;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let duration;
 const timer = new Timer(durationInput, startButton, pauseButton, {
-  onStart() {
-    console.log('Timer started!');
+  onStart(totalDuration) { //receive timeRemaining from start func
+    duration = totalDuration;
   },
-  onTick() {
-
+  onTick(timeRemaining) {
+    circle.setAttribute('stroke-dashoffset',
+      perimeter * timeRemaining / duration - perimeter // dashoffset formula
+    );
+    timeRemaining % 2 === 0 ? console.log('tack') : console.log('tick');
   },
   onComplete() {
     console.log("Time's UP!");
---
+++ Bootcamp/projects/timer/timer.js
@@ -0,0 +1,56 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    if ( callbacks ) {  //optional argument
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
+
+    this.startButton.addEventListener('click', this.start)
+    this.pauseButton.addEventListener('click', this.pause)
+  }
+
+  start = () => {
+    if (this.onStart) this.onStart(this.timeRemaining);
+    if (this.timeRemaining > 0) this.tick();
+    this.interval = setInterval(this.tick, 10);
+    this.disableStart();
+  }
+
+  pause = () => {
+    clearInterval(this.interval);
+    this.ableStart();
+  }
+
+  tick = () => {
+    if ( this.timeRemaining <= 0 ) {
+      this.pause();
+      if (this.onComplete) this.onComplete();
+    } else {
+      //calling method without the parentheses because of using get keyword
+      this.timeRemaining = this.timeRemaining - 0.01; // value automatically goes as an argument to setter call
+      if (this.onTick) this.onTick(this.timeRemaining);
+      // console.log(this.timeRemaining)
+    }
+  }
+
+  get timeRemaining() {
+    return this.durationInput.value;
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time.toFixed(2);
+  }
+
+  disableStart() {
+    this.startButton.setAttribute('disabled', 'true');
+  }
+
+  ableStart() {
+    this.startButton.removeAttribute('disabled');
+  }
+}
