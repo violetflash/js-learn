2e29e2a9a46cb697c3e7304906e350ce21840de8
Alex Grobovsky <ktotaam@mail.ru>
2021-04-09 13:40:49 +0500

    save

--- Bootcamp/projects/Vocabulary/Vocab - todo/app.js
+++ Bootcamp/projects/Vocabulary/Vocab - todo/app.js
@@ -6,12 +6,12 @@ const translationInput = document.querySelector('#form-translation');
 const addBtn = document.querySelector('.form__btn');
 const learnList = document.querySelector('.to-learn');
 const learnedList = document.querySelector('.learnedWords');
-/*
+
 const vocabData = localStorage.getItem('vocabData') ?
   JSON.parse(localStorage.getItem('vocabData')):
   {toLearn: [], learnedWords: []};
- */
-const vocabData = {toLearn: [], learnedWords: []};
+
+// const vocabData = {toLearn: [], learnedWords: []};
 
 const addElement = function(obj, word, translation) {
   obj.push({word, translation});
@@ -25,35 +25,64 @@ const render = function() {
   //Очищаем листы
   for (const elem of [learnList, learnedList]) elem.innerHTML = '';
 
-  for (const key in vocabData) {
-    let array = vocabData[key];
-    array.forEach((elem, index) => {
-      console.log(elem);
+  function renderList(obj, list, moveBtnName, moveToObjName) {
+    Object.values(obj).forEach((item, index) => {
       const li = document.createElement('li');
       li.className = 'row';
-      li.setAttribute('data-counter', `${array.length - index})`);
-      // li.innerHTML =
-      if (key === 'toLearn') {
-        li.prepend(learnList);
-      } else li.prepend(learnedList);
+      li.setAttribute('data-counter', `${obj.length - index})`);
+      li.innerHTML = `
+          <span class="word">${item.word}</span>
+          <span class="translation">${item.translation}</span>
+          <div class="controls">
+          <button class="remove-btn" type="button"></button> 
+          <button class="move-btn" type="button">Move to ${moveBtnName}</button> 
+          <a class="edit-btn">
+            <span class="tooltip">Edit</span>
+          </a> 
+          </div>
+      `;
+      list.prepend(li);
+      li.addEventListener('mouseover', function(e) {
+        document.querySelector('.controls').classList.add('is-visible');
+      });
+      li.addEventListener('mouseleave', function(e) {
+        document.querySelector('.controls').classList.remove('is-visible');
+      });
     });
   }
+
+  renderList(vocabData.toLearn, learnList, 'Learned', vocabData.learnedWords);
+  renderList(vocabData.learnedWords, learnList, 'Vocab', vocabData.toLearn);
+
 
 };
 
 const vocabulary = function(root) {
 
+  document.querySelectorAll('.form-input').forEach((elem) => {
+    elem.addEventListener('input', () => {
+      elem.classList.remove('error');
+    });
+  });
+
   addBtn.addEventListener('click', (e) => { //Добавление слова
     e.preventDefault();
+    wordInput.focus();
     const word = wordInput.value;
     const translation = translationInput.value;
-    if (word && translation) {
-      vocabData.toLearn.push({word, translation});
-      // console.log(vocabData);
-      clearFields(wordInput, translationInput);
-      localStorage.setItem('vocabData', JSON.stringify(vocabData));
-      render();
+    for (const item of [wordInput, translationInput]) {
+      if (!item.value) {
+        item.classList.add('error');
+        return;
+      }
     }
+
+    vocabData.toLearn.push({word, translation});
+    // console.log(vocabData);
+    clearFields(wordInput, translationInput);
+    localStorage.setItem('vocabData', JSON.stringify(vocabData));
+    render();
+
   });
   render();
 
--- Drafts/01/main.js
+++ Drafts/01/main.js
@@ -1,10 +1,17 @@
 
-
-for (var i = 0; i < 10; i++){
-  setTimeout(() => {
-    console.log(i);
-  }, 0)
-  setTimeout(() => {
-    console.log(i);
-  }, 1000)
+function logPerson(s, name, age) {
+  console.log( s, name, age);
+  if (age < 0) {
+    age = "так не бывает";
+  }
+  return `${s[0]}${name}${s[1]}${age}${s[2]}`;
 }
+const personName = 'Maksim';
+const personName2 = 'Tanya';
+const personAge = 35;
+const personAge2 = -36;
+
+const output = logPerson`Имя: ${personName}, Возраст:${personAge}!`;
+console.log('output: ', output);
+const output2 = logPerson`Имя: ${personName2}, Возраст:${personAge2}!`;
+console.log('output2: ', output2);
