a7cef3c485d4d20348fe96638f2db5ed8c0ebac4
Alex Grobovsky <ktotaam@mail.ru>
2021-04-30 13:45:11 +0500

    save

--- Bootcamp/projects/cards/app.js
+++ Bootcamp/projects/cards/app.js
@@ -16,6 +16,57 @@
     В случае идеального кода у претендентов - будем смотреть на стили.
 */
 
+function animate({ timing, draw, duration }) {
+    const start = performance.now();
+    requestAnimationFrame(function animate(time) {
+        // timeFraction изменяется от 0 до 1
+        let timeFraction = (time - start) / duration;
+        if (timeFraction > 1) {
+            timeFraction = 1;
+        }
+        // вычисление текущего состояния анимации
+        const progress = timing(timeFraction);
+        draw(progress); // отрисовать её
+        if (timeFraction < 1) {
+            requestAnimationFrame(animate);
+        }
+    });
+}
+
+const headerImg = document.querySelector('.header__img');
+
+const headerImgArrival = () => {
+    // animate({
+    //     duration: 2500,
+    //     timing(timeFraction) {
+    //         return 1 - Math.sin(Math.acos(timeFraction));
+    //     },
+    //     draw(progress) {
+    //         console.log(progress);
+    //         // console.log(headerImg.offsetTop);
+    //         // console.log(headerImg.offsetHeight);
+    //         if (headerImg.offsetTop < 0) {
+    //             headerImg.style.top = -headerImg.offsetHeight + progress * 1000 + 'px';
+    //         }
+    //     }
+    // });
+    let counter = 0;
+    const moveImg = () => {
+        if (headerImg.offsetTop < 0) {
+            headerImg.style.top = 10 + counter + 'px';
+        }
+        counter += 10;
+    };
+    const headerInterval = setInterval(moveImg, 1000);
+    headerImg.style.position = 'block';
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    headerImgArrival();
+
+
+});
+
 
 
 const firstReq = new XMLHttpRequest();
