37c851fba24c64f083b8babdd8df04615f877e35
Alex Grobovsky <ktotaam@mail.ru>
2021-05-04 11:10:07 +0500

    save

--- Bootcamp/toLearn/promises/app.js
+++ Bootcamp/toLearn/promises/app.js
@@ -13,42 +13,40 @@
 // });
 
 
-const fakeRequest = (url) => {
-  return new Promise((resolve, reject) => {
+const fakeRequest = url => new Promise((resolve, reject) => {
     setTimeout(() => {
-      const pages = {
-        '/users': [
-          {id: 1, username: 'Bilbo'},
-          {id: 5, username: 'Sam'}
-        ],
-        '/users/1': {
-          id: 1,
-          username: 'Bilbo',
-          upvotes: 360,
-          city: 'Lisbon',
-          topPostId: 454321,
-        },
-        '/users/5': {
-          id: 1,
-          username: 'Sam',
-          upvotes: 571,
-          city: 'Honolulu',
-        },
-        '/posts/454321': {
-          id: 454321,
-          title: 'Ladies and Gentlemen, may I introduce my pet pig, Hamlet'
-        },
-        '/about': 'This is the about page',
-      }
-      const data = pages[url];
-      if (data) {
-        resolve({status: 200, data});
-      } else {
-        reject({status: 404})
-      }
-    }, 1000)
-  })
-}
+        const pages = {
+            '/users': [
+                { id: 1, username: 'Bilbo' },
+                { id: 5, username: 'Sam' }
+            ],
+            '/users/1': {
+                id: 1,
+                username: 'Bilbo',
+                upvotes: 360,
+                city: 'Lisbon',
+                topPostId: 454321,
+            },
+            '/users/5': {
+                id: 1,
+                username: 'Sam',
+                upvotes: 571,
+                city: 'Honolulu',
+            },
+            '/posts/454321': {
+                id: 454321,
+                title: 'Ladies and Gentlemen, may I introduce my pet pig, Hamlet'
+            },
+            '/about': 'This is the about page',
+        };
+        const data = pages[url];
+        if (data) {
+            resolve({ status: 200, data });
+        } else {
+            reject({ status: 404 });
+        }
+    }, 1000);
+});
 
 
 //what a mess!
@@ -70,19 +68,19 @@ fakeRequest('/users').then((res) => {
 */
 
 fakeRequest('/users')
-  .then((res) => {
-    console.log(res)
-    const id = res.data[0].id;
-    return fakeRequest(`/users/${id}`);
-})
-  .then((res) => {
-    console.log(res)
-    const topPostId = res.data.topPostId;
-    return fakeRequest(`/posts/${topPostId}`);
-})
-  .then((res) => {
-    console.log(res);
-})
-  .catch((err) => {
-    console.log('OH NO!', err)
-  })
+    .then(res => {
+        console.log(res);
+        const id = res.data[0].id;
+        return fakeRequest(`/users/${id}`);
+    })
+    .then(res => {
+        console.log(res);
+        const topPostId = res.data.topPostId;
+        return fakeRequest(`/posts/${topPostId}`);
+    })
+    .then(res => {
+        console.log(res);
+    })
+    .catch(err => {
+        console.log('OH NO!', err);
+    });
