8b89b249bada83c84ba3e4efdf9c7f205077ca26
Alex Grobovsky <ktotaam@mail.ru>
2021-05-22 15:37:56 +0500

    initial ecomm app

--- Bootcamp/projects/ecomm/index.js
+++ Bootcamp/projects/ecomm/index.js
@@ -1,1 +1,20 @@
-console.log('hi there! again!');
+const express = require('express');
+
+const app = express();  //app - an object that describes all the things that express server can do
+
+app.get('/', (req, res) => {    //callback handler on method GET and path "/"
+    res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email" autocomplete="off">
+                <input name="password" placeholder="password" autocomplete="off">
+                <input name="passwordConfirmation" placeholder="password confirmation" autocomplete="off">
+                <button>Sign Up</button>
+            </form>
+        </div>        
+    `);
+});
+
+app.listen(3000, () => {    //telling Express to watch for incoming requests on port 3000
+    console.log('Listening');
+});
